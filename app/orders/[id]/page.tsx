dd3f38738005e7c3390b5abc18be393d8c6bfa8f
Avik2293 <aviksarker22@gmail.com>
2025-08-04 22:07:37 +0600

    vercel

--- app/orders/[id]/page.tsx
+++ app/orders/[id]/page.tsx
@@ -7,13 +7,14 @@ import { format } from 'date-fns';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
 
-interface PageProps {
-    params: {
-        id: string;
-    };
-}
+// interface PageProps {
+//     params: {
+//         id: string;
+//     };
+// }
 
-export default function OrderDetailsPage({ params }: PageProps) {
+export default function OrderDetailsPage({ params }: { params: { id: string } }) {
+    // export default function OrderDetailsPage({ params }: PageProps) {
     const { orders } = useSelector((state: RootState) => state.orders);
     const order = orders.find(o => o.id === params.id);
 
